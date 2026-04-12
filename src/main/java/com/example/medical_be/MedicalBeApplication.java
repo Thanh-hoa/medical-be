1e96331838cca1a661273eb0d8308846f14cea58
Thanh Hoa <129710515+Thanh-hoa@users.noreply.github.com>
2026-04-12 15:59:29 +0700

    init project

---
+++ src/main/java/com/example/medical_be/MedicalBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.medical_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medical_be/MedicalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medical_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
